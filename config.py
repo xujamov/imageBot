f35c7abc593bb012cb443374744dbe5fd5424c8a
doniyor <doniyorprince95@gmail.com>
2023-12-18 20:13:15 +0500

    config changes

---
+++ config.py
@@ -0,0 +1,10 @@
+# config.py
+
+import os
+from dotenv import load_dotenv
+
+# Load environment variables from .env file
+load_dotenv()
+
+TELEGRAM_BOT_TOKEN = os.getenv("TELEGRAM_BOT_TOKEN")
+GENERATIVEAI_API_KEY = os.getenv("GENERATIVEAI_API_KEY")
