9db8a3672f83e5edaf17d91fc9e5fd9844c98a37
Prince <prince777_98@mail.ru>
2024-03-29 11:22:26 +0500

    no changes

--- config.py
+++ config.py
@@ -5,6 +5,6 @@ from dotenv import load_dotenv
 
 # Load environment variables from .env file
 load_dotenv()
-
+#
 TELEGRAM_BOT_TOKEN = os.getenv("TELEGRAM_BOT_TOKEN")
 GENERATIVEAI_API_KEY = os.getenv("GENERATIVEAI_API_KEY")
